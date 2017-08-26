888470b039fedb05281a5fa799e48a2bf2e3fde6
Linder47 <catty97@mail.ru>
2017-08-26 20:34:04 +0300

    added js for main page, added second page 'photo' with layout, half done main blocks

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
- function initMap() {
-        var coordinates = {lat: 34.866910, lng: -111.792495};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: coordinates
-        });
-        var marker = new google.maps.Marker({
-          position: coordinates,
-          map: map
-        });
-      }
+var toggleClose = document.querySelector(".main-nav__toggle");
+var mainNav = document.querySelector(".main-nav");
+var toggleBurger = document.querySelector(".main-nav__toggle-logo");
+
+toggleBurger.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (mainNav.classList.contains("main-nav--closed")) {
+  mainNav.classList.remove("main-nav--closed");
+  } else {
+    mainNav.classList.add("main-nav--closed");
+  }
+});
+
+toggleClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  mainNav.classList.add("main-nav--closed");
+});
+
+
+function initMap() {
+  var coordinates = { lat: 34.866910, lng: -111.792495 },
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: coordinates
+  }),
+  marker = new google.maps.Marker({
+    position: coordinates,
+    map: map
+  });
+}
 
 /*function initMap() {
   var coordinates = { lat: 34.858599, lng: -111.795482 },
