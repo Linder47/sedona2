2ba62b3c1b9f59edb36df99dc1abc71ddf17bf1d
Linder47 <catty97@mail.ru>
2017-09-16 18:39:48 +0300

    fixed icon for map + inputs

--- js/main.js
+++ js/main.js
@@ -20,12 +20,15 @@ toggleClose.addEventListener("click", function(event) {
 function initMap() {
   var coordinates = { lat: 34.866910, lng: -111.792495 },
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 7,
     center: coordinates
   }),
+
+  markerImage = "img/map-marker.svg",
   marker = new google.maps.Marker({
     position: coordinates,
-    map: map
+    map: map,
+    icon: markerImage
   });
 }
 
